d7b097fc25fcd2b6286a95b751ae818aa73aa801
NieneB <niene.boeijen@gmail.com>
2020-08-19 10:41:22 +0200

    tiles istead of tilejson, not working

--- app/src/store/modules/data.js
+++ app/src/store/modules/data.js
@@ -1,5 +1,5 @@
 import lijst from '../../assets/gebouwen_1684.json';
-import lijst2 from '../../assets/gebouwen_1669.json';
+// import lijst2 from '../../assets/gebouwen_1669.json';
 
 import Bleau from "../../assets/mapStyleBleau_1669.json";
 import Specht from "../../assets/mapStyleSpecht_1684.json";
