0014d20023e9e46c94d04c78ca1d60cd7745bb9a
Edward Mac Gillavry <edward@webmapper.net>
2021-06-15 11:39:37 +0200

    Silence error message.

--- app/src/pages/DrieLuik.vue
+++ app/src/pages/DrieLuik.vue
@@ -31,7 +31,7 @@ export default {
         this.$router.push({
           name: "DrieLuik",
           params: { gebouw: `${this.gekozenGebouwId}` },
-        });
+        },() => {});
       }
     },
     $route(to, from) {
